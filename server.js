edbfefa2ac18291c4835bd02e7951adc2fee3459
Hawa <hawamaria33@gmail.com>
2023-07-11 20:45:18 +0300

    js

--- server.js
+++ server.js
@@ -1,24 +1,12 @@
-const jsonServer = require("json-server");
-const cors = require("cors");
-const path = require("path");
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db", "db.json"));
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3200;
 
-server.use(cors());
-server.use(jsonServer.bodyParser);
 server.use(middlewares);
-
-// Custom endpoint to serve db.json as JSON
-server.get("/api/db", (req, res) => {
-  res.setHeader("Content-Type", "application/json");
-  res.sendFile(path.join(__dirname, "db", "db.json"));
-});
-
 server.use(router);
 
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`);
-});
+console.log(`Starting server.....`)
+server.listen(port);
+console.log(`Server running in port ${port}`)
